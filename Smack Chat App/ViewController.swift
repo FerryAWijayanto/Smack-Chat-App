8481986fbd9aab10749e28e5386f6579e5aa38c7
FerryAWijayanto <ferry.adiwijayanto@outlook.com>
2022-01-10 15:29:18 +0700

    Initial commit

---
+++ Smack Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smack Chat App
+//
+//  Created by Ferry Adi Wijayanto on 10/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
